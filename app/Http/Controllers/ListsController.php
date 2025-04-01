0d7a20a6affcc0e3bd14d77ae746e18e96320715
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2025-04-01 23:29:09 +0800

    crud api setup

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -3,48 +3,64 @@
 namespace App\Http\Controllers;
 
 use App\Models\Lists;
-use App\Http\Requests\StoreListsRequest;
-use App\Http\Requests\UpdateListsRequest;
+use Illuminate\Http\Request;
 
-class ListsController extends Controller
-{
+class ListsController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index() {
         //
+        return Lists::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreListsRequest $request)
-    {
+    public function store(Request $request) {
         //
+        $fields = $request->validate([
+            'title' => 'required|max:30',
+            'body' => 'required'
+        ]);
+
+        $lists = Lists::create($fields);
+
+        return ['Lists' => $lists];
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Lists $lists)
-    {
+    public function show(Lists $list) {
         //
+        return $list;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateListsRequest $request, Lists $lists)
-    {
+    public function update(Request $request, Lists $list) {
         //
+        $fields = $request->validate([
+            'title' => 'required|max:30',
+            'body' => 'required'
+        ]);
+
+        $list->update($fields);
+
+        return ['Lists' => $list, 'Message' => "List {$list->id} successfully updated"];
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Lists $lists)
-    {
+    public function destroy(Lists $list) {
         //
+        $list->delete();
+
+        return [
+            'message' => "List {$list->id} successfully deleted"
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,4 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::get('/', function () {
-    return 'API';
-});
+Route::apiResource('lists', ListsController::class);
